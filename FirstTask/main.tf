1e67a645fad4cad4af34aadfffc01cb5e101031d
Ido Cnaan <icnaan@gmail.com>
2023-03-07 15:44:53 +0200

    authorization = "NONE"

--- FirstTask/main.tf
+++ FirstTask/main.tf
@@ -62,6 +62,7 @@ resource "aws_api_gateway_method" "example" {
   rest_api_id = aws_api_gateway_rest_api.example.id
   resource_id = aws_api_gateway_resource.example.id
   http_method = "GET"
+  authorization = "NONE"
 }
 
 resource "aws_api_gateway_integration" "example" {
