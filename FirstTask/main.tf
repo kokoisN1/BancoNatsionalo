3094632df5b528038ab8c33213edeb3ed8091708
Ido Cnaan <icnaan@gmail.com>
2023-03-08 15:44:45 +0200

    revert to direct arn

--- FirstTask/main.tf
+++ FirstTask/main.tf
@@ -71,12 +71,7 @@ resource "aws_iam_policy_attachment" "example" {
 }
 
 resource "aws_iam_role_policy_attachment" "example" {
-    for_each = toset([
-        "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole",
-        "arn:aws:iam::722738472774:role/my-lambda-role-4-s3-ro"
-      ])
-
-    policy_arn = each.value
+    policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole",
     role       = aws_iam_role.example.name
 }
 
