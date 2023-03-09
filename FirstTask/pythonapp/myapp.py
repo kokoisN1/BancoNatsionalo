254cb6c94a1a6d5ac23fe9bb811feed631f8704f
Ido Cnaan <icnaan@gmail.com>
2023-03-09 12:47:19 +0200

    another json attest

--- FirstTask/pythonapp/myapp.py
+++ FirstTask/pythonapp/myapp.py
@@ -1,5 +1,7 @@
 import boto3
 from flask import Flask
+import json
+}
 
 s3 = boto3.client('s3')
 app = Flask(__name__)
@@ -11,7 +13,9 @@ def index(event, lambda_context):
 
     response = s3.get_object(Bucket=s3_bucket_name, Key=s3_file)
     file_content = response['Body'].read().decode('utf-8')
-    return '{"statusCode":200,"headers":{"Content-Type":"application/json"},"multiValueHeaders":null,"body":"{"file content == ":"' + file_content +'"}","isBase64Encoded":false}'
+    return {'statusCode': 200,
+            'body': json.dumps({'file_content': file_content}),
+            'isBase64Encoded':false}
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
